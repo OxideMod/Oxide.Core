eeb4348a9a80d87f7f6ecb8e4edcbc1115f73ef4
Austin Vandersluis <austinv900@nindroidservers.com>
2023-12-21 17:29:20 -0800

    Add data and configuration handler classes

---
+++ src/Configuration/ConfigurationManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using Oxide.Data;
+
+namespace Oxide.Configuration
+{
+    internal class ConfigurationManager : IConfigurationManager
+    {
+        protected string ConfigDirectory { get; }
+        protected IDataReaderWriter ReaderWriter { get; }
+
+        public ConfigurationManager(string configDirectory, IDataReaderWriter rw)
+        {
+            ConfigDirectory = configDirectory ?? throw new ArgumentNullException(nameof(configDirectory));
+            ReaderWriter = rw ?? throw new ArgumentNullException(nameof(rw));
+        }
+
+        public T ReadConfig<T>(string name = null)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = DataHelpers.GetFileNameFromType<T>();
+            }
+            else
+            {
+                name = name.Replace("..", string.Empty);
+            }
+
+            return ReaderWriter.Read<T>(Path.Combine(ConfigDirectory, name));
+        }
+
+        public void WriteConfig<T>(T config, string name = null)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = DataHelpers.GetFileNameFromType<T>();
+            }
+            else
+            {
+                name = name.Replace("..", string.Empty);
+            }
+
+            ReaderWriter.Write(config, Path.Combine(ConfigDirectory, name));
+        }
+    }
+}
---
+++ src/Data/DataHelpers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+
+namespace Oxide.Data
+{
+    public static class DataHelpers
+    {
+        public static string GetFileNameFromType<T>(string extension = null) =>
+            GetFileNameFromType(typeof(T), extension);
+
+        public static string GetFileNameFromType(Type type, string extension = null)
+        {
+            string name = type.Name;
+
+            object[] attributes = type.GetCustomAttributes(typeof(DisplayNameAttribute), false);
+
+            if (attributes.Length != 0)
+            {
+                DisplayNameAttribute title = (DisplayNameAttribute)attributes[0];
+                name = title.DisplayName;
+            }
+
+
+            name = CleanFileName(name);
+
+            if (!string.IsNullOrEmpty(extension))
+            {
+                name = Path.ChangeExtension(name, extension);
+            }
+
+            return name;
+        }
+
+        public static string CleanFileName(string fileName)
+        {
+            fileName = fileName.Trim();
+
+            char[] invalids = Path.GetInvalidFileNameChars();
+
+            for (int i = 0; i < invalids.Length; i++)
+            {
+                char n = invalids[i];
+                fileName = fileName.Replace(n.ToString(), string.Empty);
+            }
+
+            return fileName;
+        }
+    }
+}
